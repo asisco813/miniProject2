fc0ae662fabdfd9b82301cf3ac6b4298ebb063b1
asisco813 <asisco813@gmail.com>
2020-10-07 13:21:55 -0400

    Initial commit

---
+++ miniProject2.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+"fmt"
+"gobot.io/x/gobot"
+"gobot.io/x/gobot/drivers/aio"
+"gobot.io/x/gobot/drivers/i2c"
+g "gobot.io/x/gobot/platforms/dexter/gopigo3"
+"gobot.io/x/gobot/platforms/raspi"
+"time"
+)
+
+func robotMainLoop(piProcessor *raspi.Adaptor, gopigo3 *g.Driver, lidarSensor *i2c.LIDARLiteDriver,
+
+) {
+
+	err := lidarSensor.Start()
+	if err != nil {
+		fmt.Println("error starting lidarSensor")
+	}
+	lidarReading, err := lidarSensor.Distance()
+	var secondCount = 0
+	for { //loop forever
+		if err != nil {
+			fmt.Println("Error reading lidar sensor %+v", err)
+		}
+		message := fmt.Sprintf("Lidar Reading: %d", lidarReading)
+
+		fmt.Println(lidarReading)
+		fmt.Println(message)
+		time.Sleep(time.Second)
+
+		for lidarReading > 100 {
+			err := gopigo3.SetMotorDps(g.MOTOR_LEFT+g.MOTOR_RIGHT, 5)
+			if err != nil {
+				fmt.Errorf("Error moving forward %+v", err)
+			}
+		}
+		for lidarReading < 100 {
+			err := gopigo3.SetMotorDps(g.MOTOR_LEFT+g.MOTOR_RIGHT, 5)
+			if err != nil {
+				fmt.Errorf("Error moving forward %+v", err)
+			}
+			secondCount += 1
+		}
+		break
+	}
+	err := gopigo3.SetMotorDps(g.MOTOR_LEFT+g.MOTOR_RIGHT, 0)
+	if err != nil {
+		fmt.Errorf("Error stopping %+v", err)
+	}
+	var lengthOfBox float64 = float64(secondCount) * 5 * .5803
+	fmt.Sprintf("The length of the box is: %d", lengthOfBox)
+
+}
+
+func main() {
+	raspberryPi := raspi.NewAdaptor()
+	gopigo3 := g.NewDriver(raspberryPi)
+	lidarSensor := i2c.NewLIDARLiteDriver(raspberryPi)
+	lightSensor := aio.NewGroveLightSensorDriver(gopigo3, "AD_2_1")
+	workerThread := func() {
+		robotMainLoop(raspberryPi, gopigo3, lidarSensor)
+	}
+	robot := gobot.NewRobot("Gopigo Pi4 Bot",
+		[]gobot.Connection{raspberryPi},
+		[]gobot.Device{gopigo3, lidarSensor, lightSensor},
+		workerThread,
+	)
+
+	err := robot.Start()
+
+	if err != nil {
+		fmt.Errorf("Error starting Robot #{err}")
+	}
+
+}
